e9f79e5054cacafb50a00013c574bd3f3cb85edb
P Singh <114702118+psingh-xyz@users.noreply.github.com>
2024-03-14 18:41:29 +0100

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -32,7 +32,7 @@ class wikiADataset(Dataset):
             for text in filtered_texts:
                 f.write(f'{text}\n')
 
-    def __init__(self, sp, input_pth='/content/ChangeTokenization/wiki_en.txt', block_size=256):
+    def __init__(self, sp, input_pth='/content/ChangeTokenization/en_wiki.txt', block_size=256):
         with open(input_pth) as f:
             data = f.readlines()
         self.examples = []
@@ -242,13 +242,64 @@ class QuoraCLFDataset(Dataset):
 
     def __getitem__(self, i):
         return torch.tensor(self.examples[i], dtype=torch.long), torch.tensor(int(self.target[i]))
-      
 
 
+class OhsumedDataset(Dataset):
+    @staticmethod
+    def _read_and_tokenize(path, sp, block_size):
+        with open(path, 'r') as f:
+            lines = f.readlines()
 
+        for i in range(len(lines)):
+            lines[i] = lines[i].strip()
 
+        text = " ".join(lines)
+        tokenized_text = sp.encode_as_ids(text)
 
+        return tokenized_text[:block_size]
 
+    @staticmethod
+    def _get_files(folder, train, split=0.8):
+        files = os.listdir(folder)
+        n = len(files)
+        if train:
+            return files[:int(split * n)]
+        else:
+            return files[int(split * n):]
+
+    @staticmethod
+    def output_sentences(path, save):
+        sentences = []
+        for folder in tqdm(os.listdir(path)):
+            for file in os.listdir(f'{path}/{folder}'):
+                with open(f'{path}/{folder}/{file}', 'r') as f:
+                    lines = f.readlines()
+                for i in range(len(lines)):
+                    lines[i] = lines[i].strip()
+
+                sentences.append(" ".join(lines))
+
+        with open(save, 'w') as f:
+            for line in sentences:
+                f.write(f'{line}\n')
+
+    def __init__(self, sp, train, block_size=256):
+        root = '/home/vdmosin/change_tokenization/datasets/ohsumed-all'
+
+        self.text = []
+        self.target = []
+
+        for i, folder in enumerate(os.listdir(root)):
+            for file in self._get_files(f'{root}/{folder}', train):
+                data = OhsumedDataset._read_and_tokenize(f'{root}/{folder}/{file}', sp, block_size)
+                self.text.append(data)
+                self.target.append(i)
+
+    def __len__(self):
+        return len(self.target)
+
+    def __getitem__(self, i):
+        return torch.tensor(self.text[i], dtype=torch.long), torch.tensor(int(self.target[i]))
 
 
 class IllnessDataset(Dataset):
@@ -275,7 +326,7 @@ class IllnessDataset(Dataset):
                 f.write(f'{line}\n')
 
     def __init__(self, sp, train, block_size=256):
-        path = '/content/ChangeTokenization/dataset/train.dat'
+        path = '/content/ChangeTokenization/medical'
 
         data = pd.read_csv(path, delimiter='\t', header=None)
         train_size = int(len(data) * 0.8)
@@ -295,3 +346,4 @@ class IllnessDataset(Dataset):
 
     def __getitem__(self, i):
         return torch.tensor(self.tokenized_text[i], dtype=torch.long), torch.tensor(int(self.target[i]))
+
